a08795aee4d9e8de7066f8ec976430309a46dbc0
Erhan <erhantezer68@gmail.com>
2022-08-05 00:33:07 +0300

    updated

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,23 +3,33 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Dashboard from "../pages/Dashboard";
 import About from "../pages/About";
 import Login from '../pages/Login';
+import NotFound from '../pages/NotFound';
+import Register from '../pages/Register';
+import Profile from '../pages/Profile';
+import NewBlog from '../pages/NewBlog';
+import Details from '../pages/Details';
+import UpdateBlog from '../pages/UpdateBlog';
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 
 const AppRouter = () => {
   return (
     <>
     <BrowserRouter>
+    <Navbar/>
     <Routes>
         <Route path="/" element={<Dashboard/>} />
         <Route path="/About" element={<About/>} />
         <Route path="/Login" element={<NotFound/>} />
         <Route path="/*" element={<Login/>} />
-        <Route path="/Login" element={<Login/>} />
-        <Route path="/Login" element={<Login/>} />
-        <Route path="/Login" element={<Login/>} />
-        <Route path="/Login" element={<Login/>} />
-        <Route path="/Login" element={<Login/>} />
+        <Route path="/Register" element={<Register/>} />
+        <Route path="/Profile" element={<Profile/>} />
+        <Route path="/NewBlog" element={<NewBlog/>} />
+        <Route path="/Details/:id" element={<Details/>} />
+        <Route path="/UpdateBlog/:id" element={<UpdateBlog/>} />
     </Routes>
+    <Footer/>
     </BrowserRouter>
     </>
   )
