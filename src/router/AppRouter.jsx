cdc9fade043a4c4f9846e0bc65549173871c6578
Erhan <erhantezer68@gmail.com>
2022-08-04 23:58:53 +0300

    updated

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,26 @@
 
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Dashboard from "../pages/Dashboard";
+import About from "../pages/About";
+import Login from '../pages/Login';
+
 
 const AppRouter = () => {
   return (
     <>
-    
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Dashboard/>} />
+        <Route path="/About" element={<About/>} />
+        <Route path="/Login" element={<NotFound/>} />
+        <Route path="/*" element={<Login/>} />
+        <Route path="/Login" element={<Login/>} />
+        <Route path="/Login" element={<Login/>} />
+        <Route path="/Login" element={<Login/>} />
+        <Route path="/Login" element={<Login/>} />
+        <Route path="/Login" element={<Login/>} />
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
