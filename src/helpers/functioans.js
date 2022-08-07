af3dd165fcf3649f6f48fdf5712a5a04ce960c24
Erhan <erhantezer68@gmail.com>
2022-08-07 16:36:34 +0300

    updated

--- src/helpers/functioans.js
+++ src/helpers/functioans.js
@@ -1,1 +1,18 @@
-// export const AddBlog = ()
+import { toastSuccessNotify } from "./ToastNotify";
+import { getDatabase,ref,push,set,onValue, remove,update } from "firebase/database";
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+export const AddBlog = (info) => {
+    const db = getDatabase();
+    const blogRef = ref(db,"blog");
+    const newBlogRef = push(blogRef);
+    Set((newBlogRef), {
+        title:info.title,
+        imageUrl:info.imageUrl,
+        email:info.email,
+        content:info.date,
+        favorites:info.favorites
+    });
+    toastSuccessNotify("Add blog successfully")
+}
